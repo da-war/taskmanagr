53bca2dec7267082f16bdb3973723020c02809ea
Rana Dawar Abdullah <imdawar090@gmail.com>
2025-01-09 05:16:24 +0500

    create task done

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) {
+    }
+    @Get()
+    getAllTasks():Task[] {
+       return this.tasksService.getAllTheTasks()
+    }
+    @Post()
+    createTask(@Body() body): Task{
+        const task = this.tasksService.createTask(body.title, body.description);
+        return task
+    }
+
+}
